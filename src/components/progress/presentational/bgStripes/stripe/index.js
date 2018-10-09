4ee2cc188b5633e6823bccf167372885e62ec63e
Carlos P Navarrete Filho <karnav@gmail.com>
2018-10-09 12:17:16 -0300

    remove unused bg components

--- src/components/progress/presentational/bgStripes/stripe/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { Stripe as StyledStripe } from './styles'
-
-const Stripe = (width) => (
-  <StyledStripe width={width} />
-)
-// REMOVE
-export default Stripe
--- src/components/progress/presentational/bgStripes/stripe/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const Stripe = styled.div`
-  width: ${props => props.width ? props.width : 100}px;
-  height: 60px;
-  background-color: yellow;
-`
-// REMOVE
